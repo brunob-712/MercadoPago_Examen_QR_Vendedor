a690e43ea8b4fa29bb4db992ad0bb564fceb1f8e
Bruno <brunob712@hotmail.com>
2020-06-28 22:36:33 -0300

    enable CORS

--- api/global/functions.php
+++ api/global/functions.php
@@ -23,7 +23,7 @@ function curl_call($method, $url, $json)
 {
     $integrator_id_test = "";
     $method = strtoupper($method);
-    $headers = array("Content-Type: application/json", "X-integrator-id: $integrator_id_test");
+    $headers = array("Content-Type: application/json", "X-integrator-id: $integrator_id_test", "Access-Control-Allow-Origin: *");
     $ch = curl_init();
     if ($method == "POST") {$post = 1;} else { $post = 0;}
     ;
